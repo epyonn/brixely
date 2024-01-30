6cd7b345411b1772b60e7f4d19a9d8d9ee5dd583
Epyonn <pdavisnguyen1@outlook.com>
2024-01-30 14:40:04 -0800

    Changed Draggable Component

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -5,7 +5,7 @@ import { BiPaperPlane } from "react-icons/bi";
 
 const Demo: React.FC = () => {  
     return (
-        <div className='relative flex justify-center items-center h-screen'>
+        <div className='relative flex justify-center items-center h-[800px]'>
             <div className='relative w-[90%] h-[100%] overflow-hidden' style={{ borderRadius: '20px' }}>
                 <video 
                     className='absolute top-0 left-0 w-full h-full object-cover'
@@ -22,8 +22,8 @@ const Demo: React.FC = () => {
                             <BiPaperPlane />
                         </span>
                     </div>
-
                 </Draggable>
+
             </div>
         </div>
     );
