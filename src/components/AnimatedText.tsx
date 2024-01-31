6ca50caf8b9b37452f7ea24e35f4b937727c392c
Epyonn <pdavisnguyen1@outlook.com>
2024-01-31 15:52:04 -0800

    Updating Font To Liga-Sans

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -3,7 +3,7 @@ import '../App.css';
 
 const AnimatedText: React.FC = () => {
     return (
-        <div className='flex mt-12 pt-8 w-4/5 justify-center'>
+        <div className='flex mt-12 pt-8 w-4/5 justify-center' style={{fontFamily: 'Liga-Sans'}}>
             <h1 className='prose prose-2xl text-7xl'>
                 Transforming the real estate sector with AI. <br /> 
                 Leya empowers law firms and legal <br /> 
