b6288e3c2737d34689d6d8dfb7838d46146abbb7
Epyonn <pdavisnguyen1@outlook.com>
2024-02-06 16:50:02 -0800

    Update Description Section

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,1 +1,27 @@
 import React from 'react';
+
+const Description: React.FC = () => {
+    const steps = [
+        {
+            title: 'Upload the docs',
+            description: 'Provide the disclosure packet for any given property into your Brixely account.'
+        },
+        {
+            title: 'Analyze w/ Brixely AI',
+            description: 'Receive a comprehensive report with pivotal details and chat assistant familiar with the property.'
+        },
+        {
+            title: 'Close the Deal',
+            description: 'Built-in intelligent checklist evaluates for risks to help you make timely decisions for yourself or your clients.'
+        }
+    ]
+
+
+
+    return (
+        <div className='flex flex-row w-[100] align-items justify-content'>
+            {}
+
+        </div>
+    )
+}
