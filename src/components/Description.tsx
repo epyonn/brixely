540f387e5707ce6366b4f71fd2292893edd01fbd
Epyonn <pdavisnguyen1@outlook.com>
2024-02-08 13:51:55 -0800

    Add Icons

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BackgroundGradient } from './ui/background-gradient';
+import { PiLightning } from 'react-icons/pi';
 import '../App.css';
 
 const Description: React.FC = () => {
     const steps = [
         {
+            icon: <PiLightning />,
             title: 'Upload the docs',
             description: 'Provide the disclosure packet for any given property into your Brixely account.'
         },
         {
+            icon: <PiLightning />,
             title: 'Analyze w/ Brixely AI',
             description: 'Receive a comprehensive report with pivotal details and chat assistant familiar with the property.'
         },
@@ -24,7 +27,7 @@ const Description: React.FC = () => {
                 <div className='m-10'>
                     <BackgroundGradient className="rounded-[22px] w-[350px] min-w-[100px] min-h-[200px] sm:p-0">
                         <div className="bg-white dark:bg-zinc-900 p-4 rounded-[20px] min-w-[100px] min-h-[200px]">
-                            {/* Use text size utilities directly for the title. Adjust the size as needed */}
+                            {step.icon}
                             <p className="text-2xl sm:text-3xl lg:text-3xl text-black mt-4 mb-2 dark:text-neutral-200">
                                {step.title}
                             </p>
