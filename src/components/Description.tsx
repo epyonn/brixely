40b4cbb0b9815303022a0c5827ab17fbd790015f
Epyonn <pdavisnguyen1@outlook.com>
2024-02-08 14:22:17 -0800

    Update Config Waitlist

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BackgroundGradient } from './ui/background-gradient';
 import { PiLightning } from 'react-icons/pi';
+import { CgFileDocument } from "react-icons/cg";
+import { FiPhone } from "react-icons/fi";
+
+
+
 import '../App.css';
 
 const Description: React.FC = () => {
     const steps = [
         {
-            icon: <PiLightning />,
+            icon: <CgFileDocument />,
             title: 'Upload the docs',
             description: 'Provide the disclosure packet for any given property into your Brixely account.'
         },
@@ -16,6 +21,7 @@ const Description: React.FC = () => {
             description: 'Receive a comprehensive report with pivotal details and chat assistant familiar with the property.'
         },
         {
+            icon: <FiPhone />,
             title: 'Close the Deal',
             description: 'Built-in intelligent checklist evaluates for risks to help you make timely decisions for yourself or your clients.'
         }
@@ -27,12 +33,15 @@ const Description: React.FC = () => {
                 <div className='m-10'>
                     <BackgroundGradient className="rounded-[22px] w-[350px] min-w-[100px] min-h-[200px] sm:p-0">
                         <div className="bg-white dark:bg-zinc-900 p-4 rounded-[20px] min-w-[100px] min-h-[200px]">
-                            {step.icon}
+                            <div className='text-2xl'>
+                                {step.icon}
+                            </div>
+                            
                             <p className="text-2xl sm:text-3xl lg:text-3xl text-black mt-4 mb-2 dark:text-neutral-200">
                                {step.title}
                             </p>
-                            {/* Keep the description text smaller for readability */}
-                            <p className="text-sm lg:text-base text-neutral-600 dark:text-neutral-400">
+
+                            <p className="text-sm lg:text-base ">
                                 {step.description}
                             </p>
                         </div>
