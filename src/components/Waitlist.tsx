bc6acad957db15fb6250523849b72bd3849647ca
Epyonn <pdavisnguyen1@outlook.com>
2024-02-09 17:16:36 -0800

    Update Waitlist Background

--- src/components/Waitlist.tsx
+++ src/components/Waitlist.tsx
@@ -5,13 +5,13 @@ import { cn } from "../utils/cn";
 
 export function Waitlist() {
   return (
-    <div className="h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+    <div className="h-screen m-10 relative flex flex-col self-center items-center justify-center antialiased rounded-lg " style={{backgroundColor:'black'}}>
+      <div className="max-w-2xl mx-auto p-4">        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-white bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+
           Join the waitlist
         </h1>
         <p></p>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+        <p className="text-white-900 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
           Welcome to MailJet, the best transactional email service on the web.
           We provide reliable, scalable, and customizable email solutions for
           your business. Whether you&apos;re sending order confirmations,
