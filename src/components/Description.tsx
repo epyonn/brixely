bead4ce6d90744ae6814e1ccbb4180098e54286f
Epyonn <pdavisnguyen1@outlook.com>
2024-02-12 15:28:58 -0800

    Change Icon Size

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -11,17 +11,17 @@ import '../App.css';
 const Description: React.FC = () => {
     const steps = [
         {
-            icon: <CgFileDocument />,
+            icon: <CgFileDocument size="25" />,
             title: 'Upload the docs',
             description: 'Provide the disclosure packet for any given property into your Brixely account.'
         },
         {
-            icon: <PiLightning />,
+            icon: <PiLightning size="25"/>,
             title: 'Analyze w/ Brixely AI',
             description: 'Receive a comprehensive report with pivotal details and chat assistant familiar with the property.'
         },
         {
-            icon: <FiPhone />,
+            icon: <FiPhone size="25" />,
             title: 'Close the Deal',
             description: 'Built-in intelligent checklist evaluates for risks to help you make timely decisions for yourself or your clients.'
         }
@@ -34,7 +34,14 @@ const Description: React.FC = () => {
                     <BackgroundGradient className="rounded-[22px] w-[350px] min-w-[100px] min-h-[200px] sm:p-0">
                         <div className="bg-white dark:bg-zinc-900 p-4 rounded-[20px] min-w-[100px] min-h-[200px]">
                             {/* Use text size utilities directly for the title. Adjust the size as needed */}
-                            {step.icon}
+                            <div> 
+                                
+                                {step.icon}
+
+
+
+                            </div>
+
                             <p className="text-2xl sm:text-3xl lg:text-3xl text-black mt-4 mb-2 dark:text-neutral-200">
                                {step.title}
                             </p>
