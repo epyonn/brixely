5c38ccac46fcb26df3bd9fac1f7dd7d790114ab9
Epyonn <pdavisnguyen1@outlook.com>
2024-02-21 17:01:36 -0800

    Update Waitlist Viewport

--- src/components/Waitlist.tsx
+++ src/components/Waitlist.tsx
@@ -5,7 +5,7 @@ import { cn } from "../utils/cn";
 
 export function Waitlist() {
   return (
-    <div className="h-screen m-10 relative flex flex-col self-center items-center justify-center antialiased rounded-2xl " style={{backgroundColor:'black'}}>
+    <div className="h-[50vh]  m-10 relative flex flex-col self-center items-center justify-center antialiased rounded-2xl " style={{backgroundColor:'black'}}>
       <div className="max-w-2xl mx-auto p-4 text-white-900">        
         <h1 className="text-white-900 relative z-10 text-lg md:text-7xl bg-clip-text text-white bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold" style={{ color: 'white' }}>
 
