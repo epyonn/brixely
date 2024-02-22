ea7496ac641d7fe7ebd6c14fe4f86982df6753fb
Epyonn <pdavisnguyen1@outlook.com>
2024-02-21 16:47:21 -0800

    Darker Header Font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,14 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header className='flex justify-between items-center h-[100px] px-20 py-8' >
-            <div className='text-black text-3xl'>
+            <div className='text-black text-3xl font-extrabold'>
                 <h1> Brixely </h1>
             </div>
-            <div className='text-black text-xl border border-black p-1 rounded-lg'>
+            <div className='text-black text-xl border border-black p-1 rounded-lg font-extrabold'>
                 <span> Log In </span>
             </div>
-            
-            
         </header>
     );
 }
-
 
 export default Header;
