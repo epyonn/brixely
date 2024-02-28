4372f74e5ae9a35667180635506571db296d690f
Epyonn <pdavisnguyen1@outlook.com>
2024-02-28 15:50:03 -0800

    Update Footer Styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,18 @@
-
 import React from 'react';
 
 const Footer:React.FC = () => {
     return (
         <div className="flex flex-col w-full h-[150px] p-10">
-            <div className="flex flex-row w-full h-[100px] items-center justify-center">
-                <div>
+            <div className="flex flex-row w-full h-[100px] items-center">
+                <div className="flex-shrink-0">
                     <h1 className='text-black text-3xl font-extrabold'> Brixely </h1>
                 </div>
-                <div>
-                    <h3>Privacy Policy</h3>
-                    <h3> Security Policy </h3>
-                    <h3> Terms of use </h3>
+                <div className="flex-grow">
+                    <div className="flex justify-center space-x-4">
+                        <h3>Privacy Policy</h3>
+                        <h3>Security Policy</h3>
+                        <h3>Terms of use</h3>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Waitlist.tsx
+++ src/components/Waitlist.tsx
@@ -8,7 +8,6 @@ export default function Waitlist() {
     <div className="h-[50vh] m-10 relative flex flex-col self-center items-center justify-center antialiased rounded-2xl " style={{ backgroundColor: 'black', opacity: 0.93}} >
       <div className="max-w-2xl mx-auto p-4 text-white-900">        
         <h1 className="text-white-900 relative z-10 text-lg md:text-7xl bg-clip-text text-white bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold" style={{ color: 'white' }}>
-
           Join the waitlist
         </h1>
         <p></p>
