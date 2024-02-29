c0f6b46bc3c15b1af3ee5102594a4393de927bcb
Epyonn <pdavisnguyen1@outlook.com>
2024-02-29 15:20:59 -0800

    Update Header/Footer Styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { ReactComponent as LogoSVG } from '../assets/images/x-social-media-logo-icon.svg';
+import { CiLock } from "react-icons/ci";
 
 const Footer: React.FC = () => {
     return (
         <div className="flex flex-col w-full h-[150px] p-10">
             
             <div className="flex flex-row w-full h-[100px] items-center justify-between">
-                <h1 className='text-black text-3xl font-extrabold flex-shrink-0'> Brixely </h1>
+                <h1 className='text-black text-3xl font-extrabold '> Brixely </h1>
                 
-                <div className="flex-grow flex justify-center items-center space-x-4">
+                <div className="flex justify-center items-center space-x-4">
                     <h3>Privacy Policy</h3>
                     <h3>Security Policy</h3>
                     <h3>Terms of use</h3>
                 </div>
-
-                <a href="https://example.com" target="_blank" rel="noopener noreferrer" className="flex-shrink-0">
+                
+                <a href="https://example.com" target="_blank" rel="noopener noreferrer" >
                     <LogoSVG className="h-8 w-8"/>
                 </a>
+
             </div>
         </div>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { CiLock } from "react-icons/ci";
+import { MdLockOutline } from "react-icons/md";
+
+
 
 const Header: React.FC = () => {
     return (
@@ -6,11 +10,12 @@ const Header: React.FC = () => {
             <div className='text-black text-3xl font-extrabold'>
                 <h1> Brixely </h1>
             </div>
-            <div className='text-black text-xl border border-black p-1 rounded-lg font-extrabold'>
+            <div className='flex items-center space-x-1 p-1.5 text-black text-xl border border-black p-1 rounded-lg font-extrabold'>
             
                 <a href="https://app.brixely.com">
                 Login
                 </a>
+                <MdLockOutline />
 
             </div>
         </header>
